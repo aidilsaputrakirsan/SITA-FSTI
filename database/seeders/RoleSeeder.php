5d8a2c44ecfb37cc7805de665c95abc1b9b762d6
Myst-ADL <89290578+aidilsaputrakirsan@users.noreply.github.com>
2025-05-03 22:24:08 +0800

    update

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Database\Seeders;
-
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
@@ -13,9 +11,13 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::create(['name' => 'mahasiswa']);
-        Role::create(['name' => 'dosen']);
-        Role::create(['name' => 'tendik']);
-        Role::create(['name' => 'koorpro']);
+        $roles = ['mahasiswa', 'dosen', 'tendik', 'koorpro'];
+        
+        foreach ($roles as $roleName) {
+            // Cek apakah role sudah ada sebelum membuatnya
+            if (!Role::where('name', $roleName)->exists()) {
+                Role::create(['name' => $roleName]);
+            }
+        }
     }
 }
